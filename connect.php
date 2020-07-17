50c1e0b35a829c0848945b95cdaf72bccce7fbbd
mr-pandy <ekerekepreciousimeh@gmail.com>
2020-07-17 14:34:00 +0100

    Corrected url for $conn php

--- connect.php
+++ connect.php
@@ -4,11 +4,11 @@ $email ="";
 if(isset($_POST['submit'])){
     $email = $_POST['email'];
     //$conn = new mysqli("localhost","root","","myemaildata");
-	$conn = new mysqli("x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+	$conn = new mysqli("nnmeqdrilkem9ked.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306",
 	 "uvekkor8ppw3s39i", "oy2hci2sbtwbhs3a", "mk9b4mpsfztl7h0x");
 	$cmd = "INSERT into email value ('$email');";
 	if(mysqli_query($conn, $cmd)){
-        echo "Successful"; 
+        echo ""; 
         // echo ""; 
 	}else{
 		"try again"; 
